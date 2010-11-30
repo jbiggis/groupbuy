bbbaaefbfb9358cc29586bfeaa41a64da574c5e9
Jonathan Chiu <Jonathan@Jonathan-PC.(none)>
2010-11-30 16:24:59 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Groupbuy::Application.config.secret_token = '13fd0de297454cfba8bcf45c0b9a3a96f160294e7f35f728b8f2eea9a98a67dc57d615dacba5b454bbeb2e8dc849ccdab387eec236a50605d5bcba90da616325'
